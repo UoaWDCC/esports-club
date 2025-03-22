3ff2e2489dfa4fe818b1f1aabc9a512d88caeb72
Tulza <Tulza0410@gmail.com>
2025-03-22 17:52:24 +1300

    feat: add drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+    out: "./drizzle",
+    schema: "./src/libs/db/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DRIZZLE_DATABASE_URL!,
+    },
+});
