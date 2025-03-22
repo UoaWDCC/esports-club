c2c4b112416a0a50748dd801d96efeaf2025ec6f
Tulza <Tulza0410@gmail.com>
2025-03-22 14:41:15 +1300

    feat: add standard layout

---
+++ src/ui/footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="content-container bg-foreground text-background p-8">
+            <p>esports website footer</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/ui/layout/StandardLayout.tsx
@@ -0,0 +1,13 @@
+import Footer from "@ui/footer/Footer";
+
+const StandardLayout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <>
+            <div className="relative min-h-dvh overflow-x-hidden p-8">
+                <main className="content-container gap-y-24">{children}</main>
+            </div>
+        </>
+    );
+};
+
+export default StandardLayout;
