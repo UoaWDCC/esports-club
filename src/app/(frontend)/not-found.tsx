c3f957d7098dbaafe84532dddc85938a550b7a3c
Subeen Ban <sban919@aucklanduni.ac.nz>
2025-05-13 12:04:50 +1200

    feat: add return home button to 404 page

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -1,7 +1,14 @@
+import Button from "@ui/button/Button";
+
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black text-white text-2xl">
-      404 - Page Not Found
+    <div className="flex items-center justify-center min-h-screen bg-black text-white text-lg">
+      <div className="flex flex-col items-center justify-center gap-10">
+        <h3>404 - Page Not Found</h3>
+        <Button href="/" variant={{ style: "solid" }}>
+          Return Home
+        </Button>
+      </div>
     </div>
   );
 }
