fced8bd57e1029244467d980b26c270cc7617102
Shazeel Ali <75409190+ShazeelAli@users.noreply.github.com>
2025-05-19 23:26:44 +1200

    feat: display table and parsing with given CSV
    Display table now shows all the members that will be added
    
    Changed CSV parsing to work with the example CSV.
    
    TODO:
    - Validation of rows and input sanitation(currently have NANs
    - DB Uploading

--- src/components/CSVReader.tsx
+++ src/components/CSVReader.tsx
@@ -3,20 +3,63 @@
 import React, { useState } from "react";
 import Papa from "papaparse";
 
-interface UserData {
-    name: string;
+interface memberData {
+    firstName: string;
+    lastName: string;
+    email: string;
+    universityId: number;
+    previousMember: boolean;
+    yearOfStudy: string;
+    gender: string;
+    ethnicity: string;
+    currentStudy: string;
 }
 
 export const CSVReader = () => {
-    const [csvData, setCsvData] = useState<UserData[]>([]);
+    const [csvData, setCSVData] = useState<memberData[]>([]);
+
+    const resultsFormat = (results: string[][]) => {
+        let tempMemberData: memberData[] = [];
+        results.map((raw, index) => {
+            if (index == 0) {
+                return;
+            }
+            let fullName: string = raw[6];
+            console.log(fullName);
+            var firstName = fullName.split(" ").slice(0, -1).join(" ");
+            var lastName = fullName.split(" ").slice(-1).join(" ");
+            let prevMemRaw = raw[3];
+            let previousMember = false;
+            if (prevMemRaw == "Yes") {
+                previousMember = true;
+            }
+
+            let newMember: memberData = {
+                firstName: firstName,
+                lastName: lastName,
+                email: raw[7],
+                universityId: Number(raw[8]),
+                previousMember: previousMember,
+                yearOfStudy: raw[12],
+                gender: raw[10],
+                ethnicity: raw[11],
+                currentStudy: raw[13],
+            };
+            tempMemberData.push(newMember);
+        });
+
+        setCSVData(tempMemberData);
+        console.log(tempMemberData);
+        console.log(results);
+    };
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0] as File;
-        Papa.parse<UserData>(file, {
-            header: true,
+        Papa.parse<string[]>(file, {
+            header: false,
+            dynamicTyping: true,
             complete: (results) => {
-                setCsvData(results.data);
-                console.log(results.data);
+                resultsFormat(results.data);
             },
         });
     };
@@ -27,13 +70,29 @@ export const CSVReader = () => {
             <table>
                 <tbody>
                     <tr>
-                        <th> Name </th>
+                        <th> First Names </th>
+                        <th> Last Name </th>
+                        <th> Email </th>
+                        <th> Member Last Year </th>
+                        <th> Student ID </th>
+                        <th> Year Of Study </th>
+                        <th> Gender </th>
+                        <th> Ethnicity </th>
+                        <th> Studying </th>
                     </tr>
 
-                    {csvData.map((user, index) => {
+                    {csvData.map((member, index) => {
                         return (
                             <tr key={index}>
-                                <td>{user.name}</td>
+                                <td>{member.firstName}</td>
+                                <td>{member.lastName}</td>
+                                <td>{member.email}</td>
+                                <td>{member.previousMember.toString()}</td>
+                                <td>{member.universityId}</td>
+                                <td>{member.yearOfStudy}</td>
+                                <td>{member.gender}</td>
+                                <td>{member.ethnicity}</td>
+                                <td>{member.currentStudy}</td>
                             </tr>
                         );
                     })}
@@ -42,3 +101,7 @@ export const CSVReader = () => {
         </div>
     );
 };
+
+function resultsFormat(result: Papa.ParseResult<string[]>) {
+    throw new Error("Function not implemented.");
+}
