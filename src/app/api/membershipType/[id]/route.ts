df46d8febf179da7b75f183c9d395fccdcaaf567
Tulza <Tulza0410@gmail.com>
2025-05-29 18:46:31 +1200

    feat: standard api response

--- src/app/api/membershipType/[id]/route.ts
+++ src/app/api/membershipType/[id]/route.ts
@@ -1,12 +1,8 @@
-import { NextResponse } from "next/server";
+import { ApiErrorResponse } from "@libs/api/responses";
 import { RouteContext, routeWrapper } from "@libs/api/wrappers";
 
 export const GET = routeWrapper(async (req, session, context: RouteContext<"id">) => {
     const { id } = await context.params;
 
-    return NextResponse.json({
-        message: "Hello from Next.js! This is a public route",
-        session,
-        req,
-    });
+    return ApiErrorResponse("bad_request", null, { id });
 });
