983816297cb7bdd4aa3f77c9adfb121c2154106f
Abbey <abbeypmz@gmail.com>
2025-07-14 23:53:47 +1200

    Feat: added top section of membership page

--- src/app/(frontend)/(protected)/profile/membership/page.tsx
+++ src/app/(frontend)/(protected)/profile/membership/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/button/Button";
 import { useProfile } from "../components/ProfileProvider";
 
 // requires user to be logged in
@@ -9,9 +10,33 @@ export default function MembershipPage() {
     const profile = useProfile();
 
     return (
-        <div className="flex h-[2000px] flex-col gap-12">
-            <h1 className="text-3xl">Memberships</h1>
-            <p>{JSON.stringify(profile, null, 2)}</p>
+        <div className="flex flex-col gap-6">
+            <h1 className="text-4xl">Membership</h1>
+            <div className="bg-gray h-[1px]"></div>
+            <h2 className="text-xl">Membership status</h2>
+            <p className="text-sm">Your membership status</p>
+            <MembershipInfoRow title = "Membership type" info = "Full year membership" />
+            <MembershipInfoRow title = "Status" info = "Active membership" />
+            <MembershipInfoRow title = "Expiry Date" info = "29/12/2025" />
+            <div className="flex justify-end gap-2">
+                <Button className="w-[238px] h-12" variant={{ style: "solid" }}><span>See advance details</span></Button>
+                <Button className="w-[238px] h-12" variant={{ style: "cta" }}><span>Scan in code</span></Button>
+            </div>
+        </div>
+        
+    );
+}
+
+interface MembershipInfoRowProps {
+    title: string;
+    info: string;
+}
+
+function MembershipInfoRow({title, info} : MembershipInfoRowProps) {
+    return (
+        <div className="flex flex-row justify-between">
+            <p> {title}: </p>
+            <p> {info} </p>
         </div>
     );
 }
