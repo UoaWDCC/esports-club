e1e3bd3b47305e1d00b4b318eb3e3522f414e70c
mattrenren <mren843@aucklanduni.ac.nz>
2025-07-16 22:15:27 +1200

    refactor: getMembershipStatus return type with zod

--- src/services/membership/getMembershipStatus.ts
+++ src/services/membership/getMembershipStatus.ts
@@ -3,11 +3,36 @@
 import { db } from "@libs/db";
 import { memberships, membershipTypes, profiles } from "@libs/db/schema";
 import { and, eq } from "drizzle-orm";
+import { z } from "zod";
+
+// data -> fe:unknown be:{data: something} ->
+// zod check type runtime -> fe:{data: something} be:{data: something}
+
+export const ZMembershipStatus = z.object({
+    body: z
+        .object({
+            valid: z.boolean(),
+        })
+        .optional(),
+    error: z.string(),
+});
+
+export type MembershipStatus = z.infer<typeof ZMembershipStatus>;
+
+// const valid = { body: { valid: true }, error: "" };
+// const invalid = { body: { bah: true }, error: "" };
+
+// valid:unknown
+// const data = ZMembershipStatus.parse(valid);
+// data:MembershipStatus
+// data.body
+
+// ZMembershipStatus.parse({});
 
 // a member is valid if there is a single active membership (current time is between a membership)
 // the membership must be paid for it to be valid
 // should return boolean
-export const getMembershipStatus = async (userId: string) => {
+export const getMembershipStatus = async (userId: string): Promise<MembershipStatus> => {
     // Find the user's profile
     // Selects id from profiles schema where userId matches the provided userId
     // To get profileId, which is just called id in the profiles schema
@@ -16,7 +41,10 @@ export const getMembershipStatus = async (userId: string) => {
         .select({ id: profiles.id })
         .from(profiles)
         .where(eq(profiles.userId, userId));
-    if (!profile[0]) return false;
+    if (!profile[0])
+        return {
+            error: "profile not found",
+        };
 
     // const profileIda = await db.select().from(profiles).where(eq(profiles.userId, userId))
     // .innerjoin(profiles, eq(users.id, profiles.id))
@@ -31,7 +59,10 @@ export const getMembershipStatus = async (userId: string) => {
         .from(memberships)
         .where(and(eq(memberships.profileId, profile[0].id), eq(memberships.isPaid, true)));
 
-    if (!membership[0]) return false;
+    if (!membership[0])
+        return {
+            error: "membership not found",
+        };
 
     // Selects the startAt and endAt from membershipTypes schema
     // where the id in membershipTypes matches the membershipTypeId we just found
@@ -43,7 +74,10 @@ export const getMembershipStatus = async (userId: string) => {
         .from(membershipTypes)
         .where(eq(membershipTypes.id, membership[0].membershipTypeId));
 
-    if (!membershipType[0]) return false;
+    if (!membershipType[0])
+        return {
+            error: "Membership type not found",
+        };
 
     // Check if current date is within the membership period
     const now = new Date();
@@ -54,13 +88,12 @@ export const getMembershipStatus = async (userId: string) => {
 
     if (!isValid)
         return {
-            body: null,
             error: "Membership is not within the start and end date",
         };
     return {
         body: {
-            isValid,
+            valid: isValid,
         },
-        error: null,
+        error: "",
     };
 };
