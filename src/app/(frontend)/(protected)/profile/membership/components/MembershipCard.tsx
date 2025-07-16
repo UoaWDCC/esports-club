454d5ee45a70141c88792ecb6d7828cddb3f25f0
Abbey <abbeypmz@gmail.com>
2025-07-16 22:01:54 +1200

    Feat: Added mobile responsiveness

--- src/app/(frontend)/(protected)/profile/membership/components/MembershipCard.tsx
+++ src/app/(frontend)/(protected)/profile/membership/components/MembershipCard.tsx
@@ -1,5 +1,7 @@
-import { Button } from "@/components/button/Button";
 import { cn } from "@libs/utils";
+
+import { Button } from "@/components/button/Button";
+
 import { TestMembershipType } from "../types/TestMembershipType";
 
 interface MembershipCardProps {
@@ -7,31 +9,45 @@ interface MembershipCardProps {
 }
 
 function formatDate(date: Date): string {
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-based
     const year = String(date.getFullYear()).slice(-2); // Last two digits
     return `${day}/${month}/${year}`;
 }
 
 export function MembershipCard({ membership }: MembershipCardProps) {
-
-    return <div className={cn(`
-         gap-6 flex w-full max-w-[752px] flex-col rounded-xl  p-3`,
-        membership.isActive ? "border border-[#978FFE] bg-[#978FFE]/20" : "bg-gray",
-    ) }>
-        <h1 className="text-xl font-bold">{membership.title}</h1>
-        <p className="text-gray-400">{membership.description}</p>
-        <div className="flex justify-between">
-            <div className="flex flex-col">
-            <p>MembershipId: {membership.id}</p>
-            <p>Purchased: {formatDate(membership.purchaseDate)}</p>
-            <p>Expiry: {formatDate(membership.expiryDate)}</p>
-            </div>
-            <div className="flex gap-2">
-            <Button className="py-3 px-[10px]" variant={{ style: "solid" }}>Active</Button>
-            <Button className="py-3 px-[10px]" variant={{ style: "cta" }}>Open invoice</Button>
+    return (
+        <div
+            className={cn(
+                `flex w-full max-w-[752px] flex-col gap-6 rounded-xl p-3`,
+                membership.isActive ? "border border-[#978FFE] bg-[#978FFE]/20" : "bg-gray",
+            )}
+        >
+            <h1 className="text-xl font-bold">{membership.title}</h1>
+            <p className="text-gray-400">{membership.description}</p>
+            <div className="flex items-end justify-between">
+                <div className="flex flex-col">
+                    <p>MembershipId: {membership.id}</p>
+                    <p>Purchased: {formatDate(membership.purchaseDate)}</p>
+                    <p>Expiry: {formatDate(membership.expiryDate)}</p>
+                </div>
+                <div className="flex h-fit gap-2">
+                    <Button
+                        className={cn(
+                            `px-[10px] py-3`,
+                            membership.isActive
+                                ? "bg-[#00FF62]/10 text-[#00FF62]"
+                                : "text-gray-400",
+                        )}
+                        variant={{ style: "solid" }}
+                    >
+                        {membership.isActive ? "Active" : "Expired"}
+                    </Button>
+                    <Button className="px-[10px] py-3" variant={{ style: "cta" }}>
+                        Open invoice
+                    </Button>
+                </div>
             </div>
         </div>
-    </div>;
+    );
 }
-
--- src/app/(frontend)/(protected)/profile/membership/page.tsx
+++ src/app/(frontend)/(protected)/profile/membership/page.tsx
@@ -1,51 +1,47 @@
 "use client";
 
 import { Button } from "@/components/button/Button";
-import { useProfile } from "../components/ProfileProvider";
-import { useState } from "react";
-import { cn } from "@libs/utils";
-import { TestMembershipType } from "./types/TestMembershipType";
-import { TestMemberships } from "./data/TestMemberships";
+
 import { MembershipCard } from "./components/MembershipCard";
 import { MembershipInfoRow } from "./components/MembershipInfoRow";
+import { TestMemberships } from "./data/TestMemberships";
 
 // requires user to be logged in
 // requires user to have a profile
 // see (protected)/profile/layout.tsx
 export default function MembershipPage() {
-    const profile = useProfile();
-
-    
-
     return (
-        <div className="flex flex-col gap-6 w-fit">
-            <h1 className="text-4xl font-tomorrow">Membership</h1>
-            <div className="bg-gray h-[1px]"></div>
-            <div className="flex flex-col gap-2">
-            <h2 className="text-xl text-[#978FFE] font-tomorrow">Membership status</h2>
-            <p className="text-sm">Your membership status</p>
+        <div className="flex items-center justify-center">
+            <div className="flex w-full max-w-[752px] flex-col gap-6">
+                <h1 className="font-tomorrow text-4xl">Membership</h1>
+                <div className="bg-gray h-[1px]"></div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="font-tomorrow text-xl text-[#978FFE]">Membership status</h2>
+                    <p className="text-sm">Your membership status</p>
+                </div>
+                <MembershipInfoRow title="Membership type" info="Full year membership" />
+                <MembershipInfoRow title="Status" info="Active membership" />
+                <MembershipInfoRow title="Expiry Date" info="29/12/2025" />
+                <div className="flex justify-end gap-2">
+                    <Button className="h-12 w-[238px]" variant={{ style: "solid" }}>
+                        <span>See advance details</span>
+                    </Button>
+                    <Button className="h-12 w-[238px]" variant={{ style: "cta" }}>
+                        <span>Scan in code</span>
+                    </Button>
+                </div>
+                <div className="bg-gray h-[1px]"></div>
+                <div className="flex flex-col gap-2">
+                    <div className="font-tomorrow flex gap-2">
+                        <h2 className="text-xl text-[#978FFE]">All membership(s)</h2>
+                        <h2 className="text-xl">({TestMemberships.length})</h2>
+                    </div>
+                    <p className="text-sm">Your membership status</p>
+                </div>
+                {TestMemberships.map((membership, index) => (
+                    <MembershipCard membership={membership} key={index} />
+                ))}
             </div>
-            <MembershipInfoRow title = "Membership type" info = "Full year membership" />
-            <MembershipInfoRow title = "Status" info = "Active membership" />
-            <MembershipInfoRow title = "Expiry Date" info = "29/12/2025" />
-            <div className="flex justify-end gap-2">
-                <Button className="w-[238px] h-12" variant={{ style: "solid" }}><span>See advance details</span></Button>
-                <Button className="w-[238px] h-12" variant={{ style: "cta" }}><span>Scan in code</span></Button>
-            </div>
-            <div className="bg-gray h-[1px]"></div>
-            <div className="flex gap-2 font-tomorrow">
-            <h2 className="text-xl text-[#978FFE]">All membership(s)</h2>
-            <h2 className="text-xl">({TestMemberships.length})</h2>
-            </div>
-            {TestMemberships.map((membership, index) => (
-                <MembershipCard membership={membership} key={index} />
-            ))}
         </div>
-        
     );
 }
-
-
-
-
-
