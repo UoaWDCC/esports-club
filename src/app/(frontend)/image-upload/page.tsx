27b0334f5beac7b210395b45d0d6898b7eaca996
Amy <ayan986@aucklanduni.ac.nz>
2025-10-12 21:43:42 +1300

    feat: added image upload component

---
+++ src/app/(frontend)/image-upload/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import UploadComponent from "@/components/image-upload/UploadComponent";
+
+export default function page() {
+    return (
+        <div>
+            <UploadComponent />
+        </div>
+    );
+}
